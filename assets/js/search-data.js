42dfc44caecd64a7dbc061f60b4cea1335ea0abc
shinrobu <92270899+shinrobu@users.noreply.github.com>
2025-03-04 01:15:20 +0000

    Deploying to gh-pages from @ shinrobu/shinrobu.github.io@cc65b2a646f81c3c9bbf311b8b499e6c8db34bd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,14 +11,14 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "Here&#39;s the collection of my projects I&#39;ve been up to.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "My GitHub repositories for projects I&#39;ve done or contributed to.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -70,9 +70,9 @@ ninja.data = [{
           section: "News",},{id: "news-launched-the-first-version-of-my-new-website",
           title: 'Launched the first version of my new website!',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-arduino-fighstick",
+          title: 'Arduino Fighstick',
+          description: "A project to create a game controller with an arcade-style button layout, colloquially known as a &quot;fightstick.&quot;",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
@@ -90,31 +90,6 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
